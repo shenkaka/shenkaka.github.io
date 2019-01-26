30dbb0d319711f03775b30bfa6f79459ef93268b
shenkaka <shenakngvip@163.com>
2019-01-26 23:24:13 +0800

    Site updated: 2019-01-26 23:23:59

---
+++ letao/public/dianshang/js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    mui('.mui-scroll-wrapper').scroll({
+        scrollY: true, //是否竖向滚动
+        scrollX: false, //是否横向滚动
+        indicators: false, //是否显示滚动条
+        bounce: true //是否启用回弹
+    });
+});
